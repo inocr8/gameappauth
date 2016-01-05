66573179788d6a3be2fe713564755a4290749dcc
Euan Walls <apps@inocr8.co>
2016-01-05 11:56:21 +0000

    set up db, with migration and also model associations

--- app/models/game.rb
+++ app/models/game.rb
@@ -1,2 +1,4 @@
 class Game < ActiveRecord::Base
+  has_many :copies
+  has_many :users, through: :copies
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,4 @@
 class User < ActiveRecord::Base
+  has_many :copies
+  has_many :games, through: :copies
 end
